3dc41f5e0a0f2a0305e8b087bd54ccba0d525dc2
Marissa <marissacooterl@gmail.com>
2020-04-07 18:02:01 -0500

    added basic styles

--- usercard/src/usercard.js
+++ usercard/src/usercard.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Usercard.css";
 
 class Usercard extends React.Component {
   render() {
     return (
-      <div>
+      <div className="Usercard">
         <p>{this.props.user.login}</p>
         <img src={this.props.user.avatar_url}/>
       </div>
